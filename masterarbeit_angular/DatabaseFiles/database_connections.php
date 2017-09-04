a3607a78142b855074749f42aa4660065e93a525
Jennyfa <4jurisch@informatik.uni-hamburg.de>
2017-09-04 19:47:34 +0200

    change databaseconnection

--- masterarbeit_angular/DatabaseFiles/database_connections.php
+++ masterarbeit_angular/DatabaseFiles/database_connections.php
@@ -2,6 +2,6 @@
 
 header("content-type: text/html; charset=UTF-8");
 // Connecting to database as mysqli_connect("hostname", "username", "password", "database name");
-$con = mysqli_connect("localhost", "root", "Kevin0203", "db_master");
+$con = mysqli_connect("localhost", "root", "admin", "db_master");
 $con->set_charset("utf8");
 ?>
